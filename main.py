553d417815b167ffe153bf10c9b1b26787d26410
Railson de Oliveira Silva <railson.de.oliveira.silva.ros@gmail.com>
2024-11-16 15:53:18 -0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#arquivo base
+#Exercício 1
+#Crie uma lista chamada minhas_frutas contendo suas frutas favoritas
+
+minhas_frutas = ["maça", "banana", "morango", "uva", "manga"] #Uma variável que recebe uma lista contendo nomes de frutas
+#Listas em Python são definidas por colchetes []
+#Dentro dos colchetes, colocamos os elementos da lista, entre aspas (simples ou duplas) que é o formato de string(elementos de texto) e separadas por vírgulas. No caso
+
+print('minhas_frutas')
